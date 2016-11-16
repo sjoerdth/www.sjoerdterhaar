75060440ad1d02551a58fbab058b1f1cf86b9520
sjoerdth <sjoerdterhaar@gmail.com>
2016-11-16 23:13:19 +0100

    updated and added a lot of styling, created block templates

--- blog/post.php
+++ blog/post.php
@@ -9,60 +9,46 @@
 <?php perch_content('header'); ?>
 
 
-<div class="m-wrapper m-wrapper--constrain">
-    <article class="m-article">
-
-        <div class="m-wrapper m-wrapper--shadow">
-
-            <div class="m-article--body">
-                <div class="text-constrain h-contains-content-list">
-                    <header>
-                        <?php perch_blog_post(perch_get('s')); ?>
-                    </header>
-
-                </div>
-            </div>
-
-        </div>
-
-    </article>
+<div class="m-wrapper m-wrapper--constrain m-wrapper--soft-sides">
+    <?php perch_blog_post(perch_get('s')); ?>
 </div>
 
 
-		    	<?php perch_blog_author_for_post(perch_get('s')); ?>
 
-		    	<div class="meta">
-		            <div class="cats">
-		                <?php perch_blog_post_categories(perch_get('s')); ?>
-		            </div>
-		            <div class="tags">
-		                <?php perch_blog_post_tags(perch_get('s')); ?>
-		            </div>
-		        </div>
+<?php perch_blog_author_for_post(perch_get('s')); ?>
 
-		    	<?php perch_blog_post_comments(perch_get('s')); ?>
+<div class="meta">
+    <div class="cats">
+        <?php perch_blog_post_categories(perch_get('s')); ?>
+    </div>
+    <div class="tags">
+        <?php perch_blog_post_tags(perch_get('s')); ?>
+    </div>
+</div>
 
-		    	<?php perch_blog_post_comment_form(perch_get('s')); ?>
+<?php perch_blog_post_comments(perch_get('s')); ?>
+
+<?php perch_blog_post_comment_form(perch_get('s')); ?>
 
 
-		<nav class="sidebar">
-		    <h2>Archive</h2>
-		    <!-- The following functions are different ways to display archives. You can use any or all of these.
+<nav class="sidebar">
+    <h2>Archive</h2>
+    <!-- The following functions are different ways to display archives. You can use any or all of these.
 
-		    All of these functions can take a parameter of a template to overwrite the default template, for example:
+    All of these functions can take a parameter of a template to overwrite the default template, for example:
 
-		    perch_blog_categories('my_template.html');
+    perch_blog_categories('my_template.html');
 
-		    -->
-		    <!--  By category listing -->
-		    <?php perch_blog_categories(); ?>
-		    <!--  By tag -->
-		    <?php perch_blog_tags(); ?>
-		    <!--  By year -->
-		    <?php perch_blog_date_archive_years(); ?>
-		    <!--  By year and then month - can take parameters for two templates. The first displays the years and the second the months see the default templates for examples -->
-		    <?php perch_blog_date_archive_months(); ?>
-    	</nav>
+    -->
+    <!--  By category listing -->
+    <?php perch_blog_categories(); ?>
+    <!--  By tag -->
+    <?php perch_blog_tags(); ?>
+    <!--  By year -->
+    <?php perch_blog_date_archive_years(); ?>
+    <!--  By year and then month - can take parameters for two templates. The first displays the years and the second the months see the default templates for examples -->
+    <?php perch_blog_date_archive_months(); ?>
+</nav>
 
 
 <?php perch_layout('global/footer'); ?>
